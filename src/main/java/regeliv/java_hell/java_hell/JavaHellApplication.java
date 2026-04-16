7b2180279ed09d18aa49d766d5bbe11b6f932447
regeliv <124085151+regeliv@users.noreply.github.com>
2026-04-16 19:31:46 +0200

    Create Java project

---
+++ src/main/java/regeliv/java_hell/java_hell/JavaHellApplication.java
@@ -0,0 +1,13 @@
+package regeliv.java_hell.java_hell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHellApplication.class, args);
+	}
+
+}
