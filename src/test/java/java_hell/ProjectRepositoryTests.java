ff0e1e32b9e1c894cc47a3bfa1a72a3fd620015d
regeliv <124085151+regeliv@users.noreply.github.com>
2026-04-25 03:01:07 +0200

    Use H2 for tests

--- src/test/java/java_hell/ProjectRepositoryTests.java
+++ src/test/java/java_hell/ProjectRepositoryTests.java
@@ -4,9 +4,9 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
-@SpringBootTest
+@DataJpaTest
 class ProjectRepositoryTest {
 
   @Autowired
