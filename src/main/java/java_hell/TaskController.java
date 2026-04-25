bced6133c4ce182b6b0c2eb9870e203358cf6868
regeliv <124085151+regeliv@users.noreply.github.com>
2026-04-25 07:42:12 +0200

    Describe API (#18)

--- src/main/java/java_hell/TaskController.java
+++ src/main/java/java_hell/TaskController.java
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
+@Tag(name = "Tasks")
 @RequestMapping("/api/tasks")
 class TaskController {
   private final TaskRepository taskRepository;
@@ -20,11 +23,13 @@ class TaskController {
   }
 
   @GetMapping
+  @Operation(summary = "Return a list of all tasks in the database")
   List<Task> all() {
     return taskRepository.findAll();
   }
 
   @PostMapping
+  @Operation(summary = "Add a task to the database")
   Task createTask(@Valid @RequestBody Task task) {
     return taskRepository.save(task);
   }
--- src/main/java/java_hell/UserController.java
+++ src/main/java/java_hell/UserController.java
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
+@Tag(name = "Users")
 @RequestMapping("/api/users")
 class UserController {
   private final UserRepository userRepository;
@@ -20,11 +23,13 @@ class UserController {
   }
 
   @GetMapping
+  @Operation(summary = "Return a list of all users in the database")
   List<User> all() {
     return userRepository.findAll();
   }
 
   @PostMapping
+  @Operation(summary = "Add a user to the database")
   User createUser(@Valid @RequestBody User user) {
     return userRepository.save(user);
   }
