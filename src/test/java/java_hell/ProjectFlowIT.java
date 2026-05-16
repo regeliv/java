4e77e7a7cdeda90178c442790d4dbd58b448456a
regeliv <124085151+regeliv@users.noreply.github.com>
2026-05-16 12:33:21 +0200

    Add empty, failing integration test (#24)

---
+++ src/test/java/java_hell/ProjectFlowIT.java
@@ -0,0 +1,12 @@
+package java_hell;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectFlowIT {
+  @Test
+  void projectFlow() {
+    fail("unimplemented");
+  }
+}
